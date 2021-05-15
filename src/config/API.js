cfe1d8331d20b07f7f46749ab2410a089a6116e6
jainpranjal0 <pranjal.j@collegepass.org>
2021-05-15 17:41:02 +0530

    UI Fix

--- src/config/API.js
+++ src/config/API.js
@@ -6,3 +6,12 @@ export var states = "https://cdn-api.co-vin.in/api/v2/admin/location/states"
 export var districts = "https://cdn-api.co-vin.in/api/v2/admin/location/districts/"
 export var calendarByDistrict = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?"
 export var genders = "https://cdn-api.co-vin.in/api/v2/registration/beneficiary/genders"
+export var newID = "https://cdn-api.co-vin.in/api/v2/registration/beneficiary/new"
+// {
+//     birth_year: "1997"
+//     consent_version: "V1"
+//     gender_id: 1
+//     name: "Pranjal Jain"
+//     photo_id_number: "466330413405"
+//     photo_id_type: 1
+// }
