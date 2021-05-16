148c4b9f16b1dc13f8bb44323b257bf5907e510f
jainpranjal0 <pranjal.j@collegepass.org>
2021-05-17 03:06:32 +0530

    Check notif on phone

--- src/App.js
+++ src/App.js
@@ -25,7 +25,15 @@ function App() {
     } else {
       Notification.requestPermission();
     }
-    setInterval(() => { showNotification() }, 10000)
+    navigator.serviceWorker.register('sw.js');
+    Notification.requestPermission(function (result) {
+      if (result === 'granted') {
+        navigator.serviceWorker.ready.then(function (registration) {
+          registration.showNotification('Notification with ServiceWorker');
+        });
+      }
+    });
+    // setInterval(() => { showNotification() }, 10000)
   }, [])
 
   const showNotification = () => {
