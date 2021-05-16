8c03363f82e9843151663e13b9fb9df7499a9b2c
jainpranjal0 <pranjal.j@collegepass.org>
2021-05-16 13:25:44 +0530

    Added mail opt

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import "./Footer.scss"
 
 import { FaTelegramPlane } from "react-icons/fa"
+import { FiMail } from "react-icons/fi"
 import { Link } from "react-router-dom"
 import React from 'react'
 
@@ -8,8 +9,11 @@ function Footer() {
     return (
         <footer>
             <div className="top">
-                <Link to="https://github.com/pranjaljain0/Vaccine-availability-tracker">Open Source</Link>
-                <Link to="https://t.me/covidvaccinetrack"><FaTelegramPlane /></Link>
+                <Link target="_blank" to="https://github.com/pranjaljain0/Vaccine-availability-tracker">Open Source</Link>
+                <div className="rightIcons">
+                    <Link to="mailto: hello@pranjaljain.me"><FiMail /></Link>
+                    <Link to="https://t.me/covidvaccinetrack"><FaTelegramPlane /></Link>
+                </div>
             </div>
             <div className="bottom">
                 <span>We stand with everyone fighting on the frontlines.</span>
