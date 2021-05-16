4bc367125c15f78fe42b4fdcb0ba67a27b216e85
jainpranjal0 <pranjal.j@collegepass.org>
2021-05-16 12:11:45 +0530

    Added info to home

--- src/pages/AuthenticatedHome/AuthenticatedHome.js
+++ src/pages/AuthenticatedHome/AuthenticatedHome.js
@@ -4,6 +4,8 @@ import React, { useEffect, useState } from 'react'
 import { beneficiaries, calendarByDistrict, districts, states, } from "../../config/API"
 
 import { AiOutlineUserAdd } from "react-icons/ai"
+import { GoInfo } from "react-icons/go"
+import { Link } from "react-router-dom"
 import NewBenModal from "../../components/Modal/NewBenModal"
 import NewSession from "../../components/Modal/NewSession"
 import axios from "axios"
@@ -63,74 +65,81 @@ function AuthenticatedHome({ state, dispatch }) {
     }
 
     return (
-        <div className="container-min">
-            <div className="beneficiaries">
-                <div className="beneficiariesHead">
-                    <span>Beneficiaries</span>
-                    <AiOutlineUserAdd className="addIcon" onClick={() => setShowAddBen(true)} />
+        <>
+            <div className="info">
+                <GoInfo className="infoIcon" />
+                <span>To book a session you need to head to <Link to="https://selfregistration.cowin.gov.in/">Cowin</Link>
+                </span>
+            </div>
+            <div className="container-min">
+                <div className="beneficiaries">
+                    <div className="beneficiariesHead">
+                        <span>Beneficiaries</span>
+                        <AiOutlineUserAdd className="addIcon" onClick={() => setShowAddBen(true)} />
+                    </div>
+                    <div className="benificiarieList">
+                        {beneficialiesList !== undefined && beneficialiesList !== null && beneficialiesList.map((item, index) => {
+                            return (<div
+                                key={index}
+                                className={classNames("benificiarieItem", newSession.beneficiaries.includes(item.beneficiary_reference_id) && "activeCell")}
+                                onClick={() => { handleBenArr(item.beneficiary_reference_id) }}>
+                                <span>{item.name}</span>
+                                <span className="subDetail">{item.birth_year}</span>
+                                <div className="vaccinatedUpdate">
+                                    <span>{item.vaccination_status}</span>
+                                </div>
+                            </div>)
+                        })}
+                    </div>
                 </div>
-                <div className="benificiarieList">
-                    {beneficialiesList !== undefined && beneficialiesList !== null && beneficialiesList.map((item, index) => {
-                        return (<div
-                            key={index}
-                            className={classNames("benificiarieItem", newSession.beneficiaries.includes(item.beneficiary_reference_id) && "activeCell")}
-                            onClick={() => { handleBenArr(item.beneficiary_reference_id) }}>
-                            <span>{item.name}</span>
-                            <span className="subDetail">{item.birth_year}</span>
-                            <div className="vaccinatedUpdate">
-                                <span>{item.vaccination_status}</span>
-                            </div>
-                        </div>)
-                    })}
+                <div className="selectors">
+                    <select
+                        defaultValue={0}
+                        onChange={e => fetchDistricts(e.target.value)}>
+                        <option value={0} disabled>Select One</option>
+                        {stateList !== undefined && stateList !== null && stateList.map((item, index) => {
+                            return (<option key={index} value={item.state_id}>{item.state_name}</option>)
+                        })}
+                    </select>
+                    <select
+                        defaultValue={0}
+                        onChange={e => fetchDistrictData(e.target.value)}>
+                        {(districtList !== undefined && districtList !== null) ? districtList.map((item, index) => {
+                            return (<option key={index} value={item.district_id}>{item.district_name}</option>)
+                        }) : <option disabled value={0}>Select State</option>}
+                    </select>
                 </div>
+                <div className="dataTable">
+                    <table>
+                        <tr>
+                            <th className="headcol">Center</th>
+                            <th>Min Age</th>
+                            <th>Availability</th>
+                            <th>pincode</th>
+                            <th>block</th>
+                            <th>vaccine</th>
+                        </tr>
+                        {centersList !== undefined && centersList !== null && centersList.map((item, index) => {
+                            // item.sessions[0].session_id
+                            return (<tr key={index} onClick={() => {
+                                setNewSession({ ...newSession, session_id: item.sessions[0].session_id })
+                                setSelSession(item)
+                                setShowNewSession(true)
+                            }}>
+                                <td className="headcol">{truncateString(item.name, 20)}</td>
+                                <td className="center">{item.sessions[0].min_age_limit}</td>
+                                <td className="center">{item.sessions[0].available_capacity}</td>
+                                <td className="center">{item.pincode}</td>
+                                <td className="center">{item.block_name}</td>
+                                <td>{item.sessions[0].vaccine}</td>
+                            </tr>)
+                        })}
+                    </table>
+                </div>
+                <NewBenModal showAddBen={showAddBen} setShowAddBen={setShowAddBen} config={config} />
+                <NewSession show={showNewSession} setShow={setShowNewSession} selSession={selSession} newSession={newSession} setNewSession={setNewSession} config={config} />
             </div>
-            <div className="selectors">
-                <select
-                    defaultValue={0}
-                    onChange={e => fetchDistricts(e.target.value)}>
-                    <option value={0} disabled>Select One</option>
-                    {stateList !== undefined && stateList !== null && stateList.map((item, index) => {
-                        return (<option key={index} value={item.state_id}>{item.state_name}</option>)
-                    })}
-                </select>
-                <select
-                    defaultValue={0}
-                    onChange={e => fetchDistrictData(e.target.value)}>
-                    {(districtList !== undefined && districtList !== null) ? districtList.map((item, index) => {
-                        return (<option key={index} value={item.district_id}>{item.district_name}</option>)
-                    }) : <option disabled value={0}>Select State</option>}
-                </select>
-            </div>
-            <div className="dataTable">
-                <table>
-                    <tr>
-                        <th className="headcol">Center</th>
-                        <th>Min Age</th>
-                        <th>Availability</th>
-                        <th>pincode</th>
-                        <th>block</th>
-                        <th>vaccine</th>
-                    </tr>
-                    {centersList !== undefined && centersList !== null && centersList.map((item, index) => {
-                        // item.sessions[0].session_id
-                        return (<tr key={index} onClick={() => {
-                            setNewSession({ ...newSession, session_id: item.sessions[0].session_id })
-                            setSelSession(item)
-                            setShowNewSession(true)
-                        }}>
-                            <td className="headcol">{truncateString(item.name, 20)}</td>
-                            <td className="center">{item.sessions[0].min_age_limit}</td>
-                            <td className="center">{item.sessions[0].available_capacity}</td>
-                            <td className="center">{item.pincode}</td>
-                            <td className="center">{item.block_name}</td>
-                            <td>{item.sessions[0].vaccine}</td>
-                        </tr>)
-                    })}
-                </table>
-            </div>
-            <NewBenModal showAddBen={showAddBen} setShowAddBen={setShowAddBen} config={config} />
-            <NewSession show={showNewSession} setShow={setShowNewSession} selSession={selSession} newSession={newSession} setNewSession={setNewSession} config={config} />
-        </div >
+        </>
     )
 }
 
